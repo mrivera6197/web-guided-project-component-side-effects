a6baa61382335af3fa08d6b1eec4c11732bd0a05
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-09 19:19:51 +0200

    wip

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,6 +7,7 @@ export default function Details(props) {
   const [details, setDetails] = useState(null)
 
   // TASK 4 - Create a side effect 🥇 that runs only after first render.
+  useEffect
 
   // TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
