e38b579c9eb553577a0f4bbf6f5aad2b90759ce6
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-09 18:55:49 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 // TASK 2 - import the contants from constants/index.js
+import {  } from '../'
 
 import Details from './Details'
 
